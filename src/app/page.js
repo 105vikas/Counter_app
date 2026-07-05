bf1980319fe1687a1b0858a42b111fd8d0e33f83
Vikas Yadav <sonu@Vikass-MacBook-Air.local>
2026-07-05 21:43:10 +0530

    add function and style

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,35 @@
 'use client'
 import { useState } from "react";
-import './page.module.css'
+import styles from './page.module.css';
 
 
 export default function Home() {
-  const [count,setCount]=useState(0);
+  const [count, setCount] = useState(0);
 
-  function counterAction(work){
-    if(work==="add"){
-      setCount(count+1);
-    }else if(work==="remove"){
-      setCount(count-1);
-    }else{
+  function counterAction(work) {
+    if (work === "add") {
+      setCount(count + 1);
+    } else if (work === "remove") {
+      if(count!==0){
+        setCount(count - 1);
+      }
+    } else {
       setCount(0)
     }
   }
 
   return (
-    <div className={"page"}>
-      <div className={"countVal"}>{count}</div>
+    <div className={styles.page}>
+      <div className={styles.heading}>Counter App</div>
 
-      <button onClick={()=>counterAction("add")}>+</button>
-      <button onClick={()=>counterAction("remove")}>-</button>
-      <button onClick={()=>counterAction()}>Reset</button>
+      <div className={styles.countVal}>{count}</div>
+
+      <div className={styles.button}>
+        <button className={`${styles.remove} ${count===0?styles.disable:""}`} onClick={() => counterAction("remove")}>-</button>
+        <button className={styles.add} onClick={() => counterAction("add")}>+</button>
+      </div>
+
+      <button className={styles.reset} onClick={() => counterAction()}>Reset</button>
     </div>
   );
 }
